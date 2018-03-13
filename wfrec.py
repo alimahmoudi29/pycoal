c48d78cbada86eae06c8dab014c59ca959d80a27
molpopgen <krthornt@uci.edu>
2018-03-12 17:13:31 -0700

    slowly moving towards recombination...

--- wfrec.py
+++ wfrec.py
@@ -1,6 +1,7 @@
 import intervaltree as it
 import msprime
 import numpy as np
+import sys
 
 
 def wfrec(nsam, rho, nsites, theta):
@@ -8,8 +9,9 @@ def wfrec(nsam, rho, nsites, theta):
     for i in range(nsam):
         samples.append(it.IntervalTree([it.Interval(0, nsites)]))
 
-    nlinks = (nsites - 1) * nsam
-    # nlinks2 = sum([i[1]-i[0]-1 for j in samples for i in j])
+    links = np.array(
+        [i[1] - i[0] - 1 for j in samples for i in j], dtype=np.int)
+    nlinks = links.sum()
 
     n = nsam
     rbp = rho / float(nsites - 1)
@@ -26,34 +28,70 @@ def wfrec(nsam, rho, nsites, theta):
 
     while(n > 1):
         rcoal = float(n * (n - 1))
+        rrec = rbp * float(nlinks)
 
-        tcoal = np.random.exponential(4. / rcoal, 1)[0]
+        iscoal = bool(np.random.random_sample(1)[0] < rcoal / (rcoal + rrec))
+        t += np.random.exponential(4. / (rcoal + rrec), 1)[0]
+        assert len(samples) == len(links), "sample/link error"
+        if iscoal is True:
+            print("coal")
+            chroms = np.sort(np.random.choice(n, 2, replace=False))
+            c1 = chroms[0]
+            c2 = chroms[1]
 
-        t += tcoal
+            nodes.add_row(time=t, flags=msprime.NODE_IS_SAMPLE)
+            for i in samples[c1]:
+                edges.add_row(left=i[0], right=i[1],
+                              parent=next_index, child=sample_indexes[c1])
+                edges.add_row(left=i[0], right=i[1],
+                              parent=next_index, child=sample_indexes[c2])
+            newchrom = it.IntervalTree()
+            for i in samples[c2]:
+                newchrom.append(i)
+            newchrom.merge_overlaps()
+            samples.pop(c2)
+            samples.pop(c1)
+            samples.append(newchrom)
+            sample_indexes.pop(c2)
+            sample_indexes.pop(c1)
+            sample_indexes.append(next_index)
+            next_index += 1
+            n -= 1
+        else:
+            print("rec")
+            # Pick a chrom proportional to
+            # its total size:
+            print("check", len(samples), len(sample_indexes), len(links))
+            chrom = np.random.choice(
+                len(sample_indexes), 1, p=links / links.sum())[0]
+            print(chrom)
+            print(len(samples[chrom]))
+            mnpos = min([i for j in samples[chrom]
+                         for i in j if i is not None])
+            mxpos = max([i for j in samples[chrom]
+                         for i in j if i is not None])
+            pos = np.random.randint(mnpos, mxpos)
+            print("to chop",samples[chrom])
+            samples[chrom].chop(pos, pos)
+            print("chopped",samples[chrom])
+            tc = it.IntervalTree([i for i in samples[chrom] if i[0] >= pos])
+            samples[chrom].remove_overlap(pos, nsites)
+            if len(samples[chrom]) == 0:
+                print(pos,tc)
+                sys.exit(0)
+            samples.append(tc)
+            sample_indexes.append(next_index)
+            print(samples)
+            next_index += 1
+            n += 1
 
-        chroms = np.sort(np.random.choice(n, 2, replace=False))
-        c1 = chroms[0]
-        c2 = chroms[1]
-
-        nodes.add_row(time=t, flags=msprime.NODE_IS_SAMPLE)
-        for i in samples[c1]:
-            edges.add_row(left=i[0], right=i[1],
-                          parent=next_index, child=sample_indexes[c1])
-            edges.add_row(left=i[0], right=i[1],
-                          parent=next_index, child=sample_indexes[c2])
-        newchrom = it.IntervalTree()
-        for i in samples[c2]:
-            newchrom.append(i)
-        newchrom.merge_overlaps()
-        samples.pop(c2)
-        samples.pop(c1)
-        samples.append(newchrom)
-        sample_indexes.pop(c2)
-        sample_indexes.pop(c1)
-        sample_indexes.append(next_index)
-        next_index += 1
-
-        n -= 1
+        assert len(samples) == len(sample_indexes), "sample/sample_index error"
+        links = np.array(
+            [i[1] - i[0] - 1 for j in samples for i in j], dtype=np.int)
+        nlinks = links.sum()
+        print(samples)
+        print(len(samples),len(links))
+        assert len(samples) == len(links), "sample/link error 2"
 
     msprime.sort_tables(nodes=nodes, edges=edges)
     return msprime.load_tables(nodes=nodes, edges=edges)
@@ -64,7 +102,7 @@ def test():
     np.random.seed(42)
     msp_rng = msprime.RandomGenerator(84)
     for i in range(1000):
-        ts = wfrec(100, 100, 1000, 100)
+        ts = wfrec(10, 100, 1000, 100)
         sites = msprime.SiteTable()
 
         mutations = msprime.MutationTable()
@@ -77,7 +115,7 @@ def test():
                             mutations=mutations)
         S.append(ts.num_mutations)
     S2 = []
-    for i in msprime.simulate(100, mutation_rate=25, num_replicates=1000):
+    for i in msprime.simulate(100, recombination_rate=25, mutation_rate=25, num_replicates=1000):
         # S2.append(i.tables.nodes[next(i.trees()).root].time)
         S2.append(i.num_mutations)
     return S, S2
